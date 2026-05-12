a77bc9429de0dc90287f2ce165471361f241780d
Rushil Rai <rushil.rai08@gmail.com>
2026-05-12 20:12:45 +0530

    Add Week 1 archived firmware examples

---
+++ week1/led_blink/main.c
@@ -0,0 +1,20 @@
+#include <ch32v00x.h>
+
+void delay(void)
+{
+    for(volatile int i = 0; i < 5000000; i++);
+}
+
+int main(void)
+{
+    RCC->APB2PCENR |= RCC_APB2Periph_GPIOD;
+
+    GPIOD->CFGLR &= ~(0xF << (4 * 6));
+    GPIOD->CFGLR |=  (0x3 << (4 * 6));
+
+    while(1)
+    {
+        GPIOD->OUTDR ^= (1 << 6);
+        delay();
+    }
+}
